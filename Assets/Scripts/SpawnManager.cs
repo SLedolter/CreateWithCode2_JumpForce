fc369d5e7090ed75ab857eb2308f3ec14293a0a6
Stefan Ledolter <s.ledolter@gmail.com>
2022-04-19 22:59:29 +0200

    SL: added continuous spawning of obstacles with InvokeRepeating

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,13 +6,20 @@ public class SpawnManager : MonoBehaviour {
   public GameObject obstaclePrefab;
   private Vector3 spawnPosition = new Vector3(30, 0, 0);
 
+  private float spawnDelay = 2;
+  private float spawnInterval = 1;
+
   // Start is called before the first frame update
   void Start() {
-    Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+    InvokeRepeating("SpawnObstacle", spawnDelay, spawnInterval);
   }
 
   // Update is called once per frame
   void Update() {
 
+  }
+
+  void SpawnObstacle() {
+    Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
   }
 }
