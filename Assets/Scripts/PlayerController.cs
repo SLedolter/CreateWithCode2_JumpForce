f2cf0ee6694f3a77f65cf5e054b422e5a0c638de
SLedolter <s.ledolter@gmail.com>
2022-04-25 21:53:04 +0200

    SL: added sounds for jumping and crashing; added BGM;

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
   private Rigidbody playerRb;
   private Animator playerAnim;
+  public ParticleSystem explosionParticle;
+  public ParticleSystem dirtParticle;
+  public AudioClip jumpSound;
+  public AudioClip crashSounnd;
+  private AudioSource playerAudio;
   public float jumpForce;
   public float gravityModifier;
   public bool isOnGround = true;
@@ -14,6 +19,7 @@ public class PlayerController : MonoBehaviour {
   void Start() {
     playerRb = GetComponent<Rigidbody>();
     playerAnim = GetComponent<Animator>();
+    playerAudio = GetComponent<AudioSource>();
     Physics.gravity *= gravityModifier;
   }
 
@@ -22,18 +28,24 @@ public class PlayerController : MonoBehaviour {
     if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver){
       playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
       isOnGround = false;
+      playerAudio.PlayOneShot(jumpSound, 1.0f);
       playerAnim.SetTrigger("Jump_trig");
+      dirtParticle.Stop();
     }
   }
 
   private void OnCollisionEnter(Collision collision) {
     if(collision.gameObject.CompareTag("Ground")){
       isOnGround = true;
+      dirtParticle.Play();
     } else if(collision.gameObject.CompareTag("Obstacle")) {
       gameOver = true;
       Debug.Log("Game over!");
+      explosionParticle.Play();
       playerAnim.SetBool("Death_b", true);
       playerAnim.SetInteger("DeathType_int", 1);
+      playerAudio.PlayOneShot(crashSounnd, 1.0f);
+      dirtParticle.Stop();
     }
   }
 }
